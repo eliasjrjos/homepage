f0b6233c9e2e018d166f6c53365433dce936e78b
Elias JRJOS <elias.jrjos@hiba.edu.sy>
2020-07-01 15:41:42 +0300

    Updates

---
+++ precache-manifest.53fca3356bb98193eb231443d79410f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfddf50459feb536854622371d77b87d",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "f219e0cac2bfbe305d85",
+    "url": "/homepage/static/css/main.2a834113.chunk.css"
+  },
+  {
+    "revision": "4b275156a897f8cde883",
+    "url": "/homepage/static/js/2.9e82942d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homepage/static/js/2.9e82942d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f219e0cac2bfbe305d85",
+    "url": "/homepage/static/js/main.2a8691c7.chunk.js"
+  },
+  {
+    "revision": "f82d803181cf5600d9fb",
+    "url": "/homepage/static/js/runtime-main.20c194bc.js"
+  },
+  {
+    "revision": "30e2a4f2730837b548df3d3bc958e382",
+    "url": "/homepage/static/media/flash.30e2a4f2.jpg"
+  }
+]);
